88a45da125fea222e7580dfdbf11a7c071593901
Felipe Mosquera <felipe298@gmail.com>
2024-04-18 13:42:30 +0200

    Creacion e implementacion de la funcion nextStep, Ejercicio 2

--- Card.jsx
+++ Card.jsx
@@ -1,8 +1,11 @@
-export const Card = ({ currentCardData }) => {
+export const Card = ({ currentCardData,nextStep }) => {
   return (
     <div>
       <h1>{currentCardData.title}</h1>
       <p>{currentCardData.description}</p>
+{/*-----------------------------Ejercicio 2 --------------------------------------------------------------*/}
+      <button onClick={nextStep}>Next</button>
+    
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { useState} from "react";
-import {Card} from '../Card';
-
+import { useState } from "react";
+import { Card } from "../Card";
 
 export function App() {
   const tutorialData = [
     {
-        title:'Decida Moltes hores',
-        description:'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-    }
-]
+      title: "Decida Moltes hores",
+      description:
+        "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
+    },
+    {
+      title: "Programa projects propis",
+      description:
+        "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials.la motivació i la implicació en el projecte ajudará a accelerar el teu aprenentatge .",
+    },
+    {
+      title: "Procura descansar",
+      description:
+        "Descansar bé i desconnectar són vitals. D´aquesta manera reduirás l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge",
+    },
+  ];
 
-const[step, setStep] = useState(0)
+  const [step, setStep] = useState(0);
+
+  const nextStep = () =>{
+    setStep (prev => prev+1)
+  }
   return (
     <>
-    
-      <Card currentCardData={tutorialData[step]} />
+      <Card currentCardData={tutorialData[step]}
+      {/*-----------------------------Ejercicio 2 --------------------------------------------------------------*/}
+      nextStep={nextStep} />
+      
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
